033edc4262037ec5790cc3f51c35a72f3b3462f2
Xubunter <xubunter@gmail.com>
2022-05-16 21:20:57 +0300

    feat: add spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Spinner",
+  components: {},
+  mounted() {
+    this.$vs.loading({
+      target: this.$el,
+      type: "scale",
+    });
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.spinner {
+  position: relative;
+  height: 100px;
+  z-index: 1;
+}
+</style>
